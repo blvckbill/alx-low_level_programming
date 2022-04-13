111621f270db7dfe05e8b446704d63e7e7fa2b7d
Abdulroqeeb Bakre <walebakre10@gmail.com>
2022-04-13 01:46:11 -0700

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _abs - converts an integer to absolute value
+ *
+ * @r: r is an ascii character
+ *
+ * Return: 0 (success)
+ */
+
+int _abs(int r)
+{
+	if (r >= 0)
+	{
+		return (r);
+	}
+	else
+	{
+		return (-r);
+	}
+}
