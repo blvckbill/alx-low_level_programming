58f0279dd9e0d1a86b480b8dbd10e9fc44c3db8b
Abdulroqeeb Bakre <walebakre10@gmail.com>
2022-04-13 02:49:38 -0700

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * add - add two integers
+ *
+ * @a: ascii character
+ *
+ * @b: ascii charcter
+ *
+ * Return: always 0
+ */
+
+int add(int a, int b)
+{
+	int sum;
+
+		sum = a + b;
+	return (sum);
+}
