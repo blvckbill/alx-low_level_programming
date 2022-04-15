bcfe20f9698eb5791488811fa0dd05870ac297d4
Abdulroqeeb Bakre <walebakre10@gmail.com>
2022-04-15 07:47:36 -0700

    task 10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints triangke
+ * @size: size of triangle
+ *
+ * Return: always 0
+ */
+
+void print_triangle(int size)
+{
+	int row, column, k;
+
+	if (size <= o)
+		_putchar('\n');
+	for (row = 0; row < size; row++)
+	{
+		for (column = size - row; column > 1; column--)
+			_putchar(' ');
+		for (k = row = column; k >= 1; k--)
+			_putchar('#');
+		_putchar('\n');
+	}
+}
