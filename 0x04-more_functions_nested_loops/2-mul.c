9b1e30b3a3900f1eca946b0a145fe2ab1e9e90c2
Abdulroqeeb Bakre <walebakre10@gmail.com>
2022-04-15 05:50:15 -0700

    task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - to multiply two integers
+ *
+ * @a: ascii character
+ * @b: ascii character
+ *
+ * Return: always 0
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
