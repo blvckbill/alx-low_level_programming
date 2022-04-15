ab8106d6d2c2ff5d2c65d89b92b108fdaee441b3
Abdulroqeeb Bakre <walebakre10@gmail.com>
2022-04-15 05:28:01 -0700

    task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _isupper - prints upper letters
+ *
+ * @c: ascii character
+ * Return: 1 if upper
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else if (c <= 'z')
+	{
+		return (0);
+	}
+	else
+	{
+		return (0);
+	}
+	
+}
