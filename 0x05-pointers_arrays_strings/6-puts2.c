c737102b8117c64c6b3fd6aee37f7528ad05acab
Abdulroqeeb Bakre <walebakre10@gmail.com>
2022-04-20 01:51:41 -0700

    task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * puts2 - prints every other character
+ * @str: ascii character
+ *
+ * Return: always 0
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+
+		i++;
+	}
+	_putchar('\n');
+}
