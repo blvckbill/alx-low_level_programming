b4a140c13261045264c1046cfeb42127efd0e676
Abdulroqeeb Bakre <walebakre10@gmail.com>
2022-04-20 01:30:11 -0700

    task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - prints a string
+ * @str: ascii character
+ *
+ * Return: always 0
+ */
+
+void _puts(char *str)
+{
+	while(*str != '\0')
+	{
+		_putchar(*str++);
+	}
+
+		_putchar('\n');
+}
