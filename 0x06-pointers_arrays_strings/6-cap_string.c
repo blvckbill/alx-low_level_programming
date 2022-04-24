0b30a3edf808253de42653cb6fb27358bde48327
Abdulroqeeb Bakre <walebakre10@gmail.com>
2022-04-24 04:24:20 -0700

    task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -20,9 +20,9 @@ int isDelimiter(char c)
 {
 	int i;
 
-	char delimiter[] = " \t\n,.!?\"(){}";
+	char delimiter[] = " \t\n,;.!?\"(){}";
 
-	for (i = 0; i < 12; i++)
+	for (i = 0; i < 13; i++)
 		if (c == delimiter[i])
 			return (1);
 	return (0);
