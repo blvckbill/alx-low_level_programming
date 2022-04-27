05a92fef2d86a13cf8680f4c01a047452eb085f8
Abdulroqeeb Bakre <walebakre10@gmail.com>
2022-04-27 15:13:30 -0700

    task 4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strpbrk - search a string for any set of bytes
+ * @s: string
+ * @accept: string to match
+ * Return: 0
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+	char *p;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		j = 0;
+		while (accept[j] != '\0')
+		{
+			if (accept[j] == s[i])
+			{
+				p = &s[i];
+				return (p);
+			}
+			j++;
+		}
+		i++;
+	}
+	return (0);
+}
