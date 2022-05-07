d2bb04d4adc980c7b34ac0eb6d3c072c9650a48f
Abdulroqeeb Bakre <walebakre10@gmail.com>
2022-05-07 13:16:18 -0700

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - program prints name followed by a newline
+ * @argc: int
+ * @argv: list
+ * Return: 0
+ */
+
+int main(int argc, char const *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
